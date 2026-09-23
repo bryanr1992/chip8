f4cf2017726d07c5a383b747dc62152c317708be
bryanr1992 <bryan.rivas05@gmail.com>
2026-09-23 14:45:27 -0400

    Implemented loading ROM instructions into memory

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,40 @@ public:
     uint16_t index{}; // index register to store mem address. Max address is 0xFFF (12-bits) so we need 16 bits to cover lvl
     uint16_t opcode;
     uint32_t screen[64 * 32]{};
+
+    void LoadRom(const char* filename);
+
 };
+
+//TODO: Abstract Load ROM into its own CPP FILE 
+
+//starting address of rom instructions
+const unsigned int START_ADDRESS = 0x200;
+
+    //load rom
+void Chip8::LoadRom(const char* filename) {
+        
+    //open rom file as a stream of binary data and move the file pointer to EOF
+    std::ifstream file(filename, std::ios::binary | std::ios::ate);
+
+    if(file.is_open()) {
+        std::streampos size = file.tellg(); //get size of file
+        char* buffer = new char[size]; //allocate buffer based on size
+
+        //go back to file beginning and load the buffer with data
+        file.seekg(0, std::ios::beg);
+        file.read(buffer, size);
+        file.close();
+
+        //load the contents of the ROM into the CHIP8 memory
+
+        for(long i = 0; i < size; i++) {
+            memory[START_ADDRESS + i] = buffer[i];
+        }
+        //free the buffer
+        delete[] buffer;
+    }
+}
 
 int main() {
     std::cout << "Hello World of Emulators" << std::endl;
