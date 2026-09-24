eff6eebf271ca226ec4fdc20f1e439b4ea0f173a
bryanr1992 <bryan.rivas05@gmail.com>
2026-09-24 16:04:21 -0400

    Seperated function declaration and definition for LoadRom by creating a header file for Chip8. Use variable names for sizes instead of hard coded values for clarity

--- src/Chip8.cpp
+++ src/Chip8.cpp
@@ -1,31 +1,11 @@
-#include <cstdint>
+#include "Chip8.h"
 #include <iostream>
 #include <fstream>
 
-class Chip8 {
-public:
-    uint8_t memory[4096]{}; //4KB memory 1 byte each address 0-4095
-    uint8_t registers[16]{}; //16 8-bit registers
-    uint8_t sp{}; //stack pointer. We have a 16-lvl stack that can be index with 0-15
-    uint8_t delayTimer{};
-    uint8_t audioTimer{};
-    uint8_t keypad[16]{}; //16 keys keypad
-    uint16_t stack[16]{};
-    uint16_t pc{}; //program pointer
-    uint16_t index{}; // index register to store mem address. Max address is 0xFFF (12-bits) so we need 16 bits to cover lvl
-    uint16_t opcode;
-    uint32_t screen[64 * 32]{};
-
-    void LoadRom(const char* filename);
-
-};
-
-//TODO: Abstract Load ROM into its own CPP FILE 
-
-//starting address of rom instructions
+// starting address of ROM instructions
 const unsigned int START_ADDRESS = 0x200;
-
-    //load rom
+ 
+//load rom
 void Chip8::LoadRom(const char* filename) {
         
     //open rom file as a stream of binary data and move the file pointer to EOF
@@ -43,7 +23,7 @@ void Chip8::LoadRom(const char* filename) {
         //load the contents of the ROM into the CHIP8 memory
 
         for(long i = 0; i < size; i++) {
-            memory[START_ADDRESS + i] = buffer[i];
+            m_Memory[START_ADDRESS + i] = buffer[i];
         }
         //free the buffer
         delete[] buffer;
---
+++ src/Chip8.h
@@ -0,0 +1,29 @@
+#include <cstdint>
+
+//Not sure if this types need to be 4 bytes in size yet
+const unsigned int MEMORY_SIZE = 4096;
+const unsigned int KEYPAD_COUNT = 16;
+const unsigned int REGISTER_COUNT = 16;
+const unsigned int STACK_LEVEL = 16;
+const unsigned int SCREEN_WIDTH = 64;
+const unsigned int SCREEN_HEIGHT = 32;
+
+class Chip8 {
+private:
+    uint8_t m_Memory[MEMORY_SIZE]; //4KB memory 1 byte each address 0-4095
+    uint8_t mRegisters[REGISTER_COUNT]; //16 8-bit registers
+    uint8_t m_Sp{}; //stack pointer. We have a 16-lvl stack that can be index with 0-15
+    uint8_t m_DelayTimer{};
+    uint8_t m_AudioTimer{};
+    uint16_t m_Stack[STACK_LEVEL]{};
+    uint16_t m_Pc{}; //program pointer
+    uint16_t m_Index{}; // index register to store mem address. Max address is 0xFFF (12-bits) so we need 16 bits to cover lvl
+    uint16_t m_Opcode;
+    
+public:
+    uint8_t m_Keypad[KEYPAD_COUNT]{}; //16 keys keypad
+    uint32_t m_screen[SCREEN_WIDTH * SCREEN_HEIGHT]{};
+
+public:
+    void LoadRom(const char* filename);
+};
